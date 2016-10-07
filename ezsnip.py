e0e22b724881e049c3536cbdb73250d213e1cdb2
Jyry Hjelt <hjelt.jyry@gmail.com>
2016-10-07 19:31:03 +0200

    Added file creating and header file support

--- ezsnip.py
+++ ezsnip.py
@@ -2,16 +2,29 @@
 
 import sys
 import os
-
+from platform import system
 
 print("Creating File!")
 
+print("Windows")
 if sys.argv[1] == "cpp":
-        print("CPP!")
+    #print("CPP!")
+    FILE = open("%s.cpp" % sys.argv[2], 'a')
+    FILE.close()
+elif (sys.argv[1] == "header") | (sys.argv[1] == "h"):
+    #print("Header")
+    FILE = open("%s.h" % sys.argv[2], 'a')
 elif sys.argv[1] == "ruby":
-        print("ruby")
+    #print("ruby")
+    FILE = open("%s.rb" % sys.argv[2], 'a')
+    FILE.close()
 elif sys.argv[1] == "c":
-        print("C")
+    #print("C")
+    FILE = open("%s.c" % sys.argv[2], 'a')
+    FILE.close()
 elif sys.argv[1] == "python":
-        print("Python")
+    #print("Python"
+    FILE = open("%s.py" % sys.argv[2], 'a')
+    FILE.close()
+
 
