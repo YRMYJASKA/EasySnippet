18b2194d78ff8ed83a203bc155bc375427328130
Jyry Hjelt <hjelt.jyry@gmail.com>
2016-10-07 18:27:58 +0200

    Last one was an accidental commit

--- ezsnip.py
+++ ezsnip.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
 
-print("moi")
+import sys
+import os
+
+
+print("Creating File!")
+
+if sys.argv[1] == "cpp":
+        print("CPP!")
+elif sys.argv[1] == "ruby":
+        print("ruby")
+elif sys.argv[1] == "c":
+        print("C")
+elif sys.argv[1] == "python":
+        print("Python")
 
