2ee6c3de795382f1e07ad6cb2bdac7f65d3cdf63
Jyry Hjelt <hjelt.jyry@gmail.com>
2016-10-08 23:34:56 +0200

    Worked some more on the snippets.py file and snippets

--- ezsnip.py
+++ ezsnip.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python
 
-import json
 import snippets
 import sys
 import os
+
+#Refrence to the programs arguments:
+#argv[1] = Language
+#argv[2] = Snippets name
+#argv[3] = Files name
+
 #print("Creating File!")
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     print("Too few arguments. See ezsnip -help for help")
 elif sys.argv[1] == "cpp":
     #print("CPP!")
-    FILE = open("%s.cpp" % sys.argv[2], 'a')
+    FILE = open("%s.cpp" % sys.argv[3], 'a')
+    snippets.WriteSnippet("cpp", FILE, sys.argv[2])
     FILE.close()
 elif (sys.argv[1] == "header") | (sys.argv[1] == "h"):
     #print("Header")
-    FILE = open("%s.h" % sys.argv[2], 'a')
+    FILE = open("%s.h" % sys.argv[3], 'a')
 elif (sys.argv[1] == "ruby") |  (sys.argv[1] == "rb"):
     #print("ruby")
-    FILE = open("%s.rb" % sys.argv[2], 'a')
+    FILE = open("%s.rb" % sys.argv[3], 'a')
     FILE.close()
 elif sys.argv[1] == "c":
     #print("C")
-    FILE = open("%s.c" % sys.argv[2], 'a')
+    FILE = open("%s.c" % sys.argv[3], 'a')
     FILE.close()
 elif (sys.argv[1] == "python") |  (sys.argv[1] == "py"):
     #print("Python"
-    FILE = open("%s.py" % sys.argv[2], 'a')
+    FILE = open("%s.py" % sys.argv[3], 'a')
     FILE.close()
 elif sys.argv[1] == "-help":
     print("help")
--- snippets.py
+++ snippets.py
@@ -1,7 +1,20 @@
 import os
-for file in os.listdir("Snippets/Cpp"):
-    if file.endswith(".ezsnip"):
-        print(file)
-def WriteSnippet(snip, fileName):
-    
+def getSnips(directory, outfile):
+    for file in os.listdir(directory):
+        if file.endswith(".ezsnip"):
+            print(file)       
+   
+def WriteSnippet(lang, outfile, snip):
+    if lang == "cpp":
+        getSnips("Snippets/Cpp", outfile)
+    elif lang == "header":
+        getSnips("Snippets/Header", outfile)
+    elif lang == "ruby":
+        getSnips("Snippets/Ruby", outfile)
+    elif lang == "c":
+        getSnips("Snippets/C", )
+    elif lang == "python":
+        getSnips("Snippets/Python", outfile)
+    else:
+        print("Snippets not found")
     return
