cd8f8e4dc41b189a9533d9ce5750a121846915ed
Jyry Hjelt <hjelt.jyry@gmail.com>
2016-10-09 10:42:21 +0200

    Not working but not giving any error either. We are close.

--- ezsnip.py
+++ ezsnip.py
@@ -6,32 +6,28 @@ import os
 
 #Refrence to the programs arguments:
 #argv[1] = Language
-#argv[2] = Snippets name
-#argv[3] = Files name
+#argv[2] = Snippet
+#argv[3] = File name
 
 #print("Creating File!")
 if len(sys.argv) < 3:
     print("Too few arguments. See ezsnip -help for help")
 elif sys.argv[1] == "cpp":
     #print("CPP!")
-    FILE = open("%s.cpp" % sys.argv[3], 'a')
+    FILE = "%s.cpp" % sys.argv[3]
     snippets.WriteSnippet("cpp", FILE, sys.argv[2])
-    FILE.close()
 elif (sys.argv[1] == "header") | (sys.argv[1] == "h"):
     #print("Header")
-    FILE = open("%s.h" % sys.argv[3], 'a')
+    FILE = "%s.h" % sys.argv[3] 
 elif (sys.argv[1] == "ruby") |  (sys.argv[1] == "rb"):
     #print("ruby")
-    FILE = open("%s.rb" % sys.argv[3], 'a')
-    FILE.close()
+    FILE = "%s.rb" % sys.argv[3] 
 elif sys.argv[1] == "c":
     #print("C")
-    FILE = open("%s.c" % sys.argv[3], 'a')
-    FILE.close()
+    FILE = "%s.c" % sys.argv[3] 
 elif (sys.argv[1] == "python") |  (sys.argv[1] == "py"):
     #print("Python"
-    FILE = open("%s.py" % sys.argv[3], 'a')
-    FILE.close()
+    FILE = "%s.py" % sys.argv[3] 
 elif sys.argv[1] == "-help":
     print("help")
 else:
--- snippets.py
+++ snippets.py
@@ -1,20 +1,35 @@
 import os
-def getSnips(directory, outfile):
+def getSnips(directory):
+    snipList = []
     for file in os.listdir(directory):
         if file.endswith(".ezsnip"):
-            print(file)       
+            print(file)
+            snipList.append(str(file))
+    return snipList
    
 def WriteSnippet(lang, outfile, snip):
+    FILE = open(outfile, 'w')
     if lang == "cpp":
-        getSnips("Snippets/Cpp", outfile)
+        snippetList = getSnips("Snippets/Cpp")
+        for x in range(0, len(snippetList)):
+            if snippetList[x] == "%s.ezsnip" % snip:
+                infile = open(snippetList[x], 'r')
+                for line in infile:
+                    FILE.write(line)
+                infile.close()
+                break
+                
     elif lang == "header":
-        getSnips("Snippets/Header", outfile)
+        getSnips("Snippets/Header")
     elif lang == "ruby":
-        getSnips("Snippets/Ruby", outfile)
+        getSnips("Snippets/Ruby")
     elif lang == "c":
         getSnips("Snippets/C", )
     elif lang == "python":
-        getSnips("Snippets/Python", outfile)
+        getSnips("Snippets/Python")
     else:
         print("Snippets not found")
+    FILE.close()
     return
+
+WriteSnippet("cpp", "moi.cpp", "class")
